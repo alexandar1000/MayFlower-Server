192d73f0a5b0653c8f8bb2b37e9e2f520cd70dd0
Alexandar <alexandar1000@gmail.com>
2020-07-20 22:32:19 +0100

    Update the structure of the server project

---
+++ mayflower_server/mayflower_server/thermometer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ThermometerConfig(AppConfig):
+    name = 'thermometer'
--- mayflower_server/mayflower_server/urls.py
+++ mayflower_server/mayflower_server/urls.py
@@ -1,4 +1,4 @@
-"""mayflowerserver URL Configuration
+"""mayflower_server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
