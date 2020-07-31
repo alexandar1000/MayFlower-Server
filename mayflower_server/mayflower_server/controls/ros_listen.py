858d967c5a78620426462c24c70d3b07e12cf629
Roman Matios <kmlnaib@gmail.com>
2020-07-31 22:08:36 +0100

    dummy testing ros connection

--- mayflower_server/mayflower_server/controls/ros_listen.py
+++ mayflower_server/mayflower_server/controls/ros_listen.py
@@ -1,11 +1,12 @@
 from __future__ import print_function
 import roslibpy
 
-client = roslibpy.Ros(host='parallels', port=9090)
+client = roslibpy.Ros(host='10.211.55.4', port=9090)
 client.run()
 
-listener = roslibpy.Topic(client, '/boat_controls', 'std_msgs/String')
+listener = roslibpy.Topic(client, '/controls', 'std_msgs/String')
 listener.subscribe(lambda message: print('Heard talking: ' + message['data']))
+
 
 try:
     while True:
--- mayflower_server/mayflower_server/controls/ros_publish.py
+++ mayflower_server/mayflower_server/controls/ros_publish.py
@@ -3,47 +3,52 @@ from datetime import datetime
 
 import roslibpy
 
-# client = roslibpy.Ros(host='parallels', port=9090)
-# client.run()
+client = roslibpy.Ros(host='parallels', port=9090)
+client.run()
+
+talker = roslibpy.Topic(client, '/controls', 'std_msgs/String')
+
+counter = 0
+
+try:
+    while client.is_connected:
+        data = {'data': f'wasd {counter}'}
+        counter += 1
+        talker.publish(roslibpy.Message(data))
+        print(f'{counter} - Sending message...', data)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    talker.unadvertise()
+    client.terminate()
+
+
+# class RosControlsPublisher:
 #
-# talker = roslibpy.Topic(client, '/chatter', 'std_msgs/String')
+#     def __init__(self, ros_host='10.211.55.4', topic='/controls'):
+#         self.host = ros_host
+#         self.port = 9090
+#         self.client = roslibpy.Ros(host=self.host, port=9090)
+#         self.client.run()
+#         self.talker = roslibpy.Topic(self.client, topic, 'std_msgs/String')
+#         self.talker.advertise()
 #
-# while client.is_connected:
-#     data = {'data': 'wasd'}
-#     talker.publish(roslibpy.Message(data))
-#     print('Sending message...', data)
-#     time.sleep(1)
+#     def is_connected(self):
+#         if self.client.is_connected:
+#             return True
+#         return False
 #
-# talker.unadvertise()
+#     def send_command(self, command):
+#         valid_commands = 'wasdWASD'
+#         if command not in valid_commands:
+#             return False
 #
-# client.terminate()
-
-
-class RosControlsPublisher:
-
-    def __init__(self, ros_host='parallels', topic='/boat_controls'):
-        self.host = ros_host
-        self.port = 9090
-        self.client = roslibpy.Ros(host=self.host, port=9090)
-        self.client.run()
-        self.talker = roslibpy.Topic(self.client, topic, 'std_msgs/String')
-
-    def is_connected(self):
-        if self.client.is_connected:
-            return True
-        return False
-
-    def send_command(self, command):
-        valid_commands = 'wasdWASD'
-        if command not in valid_commands:
-            return False
-
-        if self.client.is_connected:
-            data = {'data': command.lower()}
-            self.talker.publish(roslibpy.Message(data))
-            print(f'Message: "{data}" at {datetime.now()}')
-            return True
-
-    def disconnect(self):
-        self.talker.unadvertise()
-        self.client.terminate()
+#         if self.client.is_connected:
+#             data = {'data': command.lower()}
+#             self.talker.publish(roslibpy.Message(data))
+#             print(f'Message: "{data}" at {datetime.now()}')
+#             return True
+#
+#     def disconnect(self):
+#         self.talker.unadvertise()
+#         self.client.terminate()
